27bd6d84436a0d50e59e6efd21c1f54f18abd5ac
chien <chientranminh355@gmail.com>
2024-09-16 21:40:20 +0700

    bai tap tuan 2

--- baitaptuan2/bai2.java
+++ baitaptuan2/bai2.java
@@ -2,10 +2,13 @@ package baitaptuan2;
 
 public class bai2 {
     public static void main(String[] args) {
-        char name;
-        int age;
-        double height;
-        boolean check;
-        
+        String name = "tran minh chien";
+        int age = 18;
+        double height = 1.7;
+        boolean thichlaptrinh = true;
+        System.out.println("ten: " +name);
+        System.out.println("tuoi: " + age);
+        System.out.println("chieu cao: " +height);
+        System.out.println("co thich lap trinh khong: " + thichlaptrinh);
     }
 }
