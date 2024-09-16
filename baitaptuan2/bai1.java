ec80eb7ab859350e1a48e9dd1aec22b0ad0e7b59
chien <chientranminh355@gmail.com>
2024-09-16 21:29:10 +0700

    bai tap tuan 2

---
+++ baitaptuan2/bai1.java
@@ -0,0 +1,13 @@
+package baitaptuan2;
+
+import java.util.Scanner;
+
+public class bai1
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap ten: ");
+        String  name = sc.nextLine();
+        System.out.println("Hello "+name);
+    }
+}
---
+++ baitaptuan2/bai2.java
@@ -0,0 +1,11 @@
+package baitaptuan2;
+
+public class bai2 {
+    public static void main(String[] args) {
+        char name;
+        int age;
+        double height;
+        boolean check;
+        
+    }
+}
